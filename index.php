2aa65980ce387496a8847375029864a3b84dee69
Heidi Mason <heidi@musiclaps.com>
2014-11-18 10:47:19 -0800

    Edit positioning/dimensions per Masa, add to 'Ranking' section, refactor

--- index.php
+++ index.php
@@ -12,13 +12,14 @@
 			<img class="cherry-blossoms" src="<?php echo BASE_URL; ?>assets/images/cherry-blossoms-top-left.png" alt="cherry blossoms">
 			
 			<div id="youtube-player-placeholder"></div><!-- Placeholder -->
+			<div id="youtube-player-advanced-placeholder"></div><!-- Placeholder -->
 			
 			<img class="cherry-blossoms" src="<?php echo BASE_URL; ?>assets/images/cherry-blossoms-bottom-right.png" alt="cherry blossoms">
 		</div>
 
 		<div class="row col-xs-12">
 			<div class="row">
-				<div class="col-xs-12 col-sm-10">
+				<div class="col-xs-12 col-sm-9">
 					<h1>Most Viewed</h1>
 					
 					<div class="col-xs-12 col-xs-4">
@@ -46,42 +47,46 @@
 					</div>
 				</div>
 
-				<div class="col-xs-12 col-sm-2">
+				<div class="col-xs-12 col-sm-3">
 					<h1 id="ranking">Ranking</h1>
-
-				</div>
-			</div>
-
-			<div class="row">
-				<div class="col-xs-12 col-sm-10">
-					<div class="col-xs-12 col-xs-4">
-						<div class="video-thumbnail-container">
-							<div class="video-thumbnail-placeholder"></div>
-						</div><!-- Placeholder -->
-						<h4 class="video-title">Video Title</h4>
-						<h4>Video description...</h4>
-					</div>
-
-					<div class="col-xs-12 col-xs-4">
-						<div class="video-thumbnail-container">
-							<div class="video-thumbnail-placeholder"></div>
-						</div><!-- Placeholder -->
-						<h4 class="video-title">Video Title</h4>
-						<h4>Video description...</h4>
-					</div>
-
-					<div class="col-xs-12 col-xs-4">
-						<div class="video-thumbnail-container">
-							<div class="video-thumbnail-placeholder"></div>
-						</div><!-- Placeholder -->
-						<h4 class="video-title">Video Title</h4>
-						<h4>Video description...</h4>
+					
+					<div class="row">
+						<h2>#1</h2>
+						<img src="<?php echo BASE_URL; ?>assets/images/avatar.jpg" alt="avatar">
 					</div>
 				</div>
 			</div>
 
 			<div class="row">
-				<div class="col-xs-12 col-sm-10">
+				<div class="col-xs-12 col-sm-9">
+					<div class="col-xs-12 col-xs-4">
+						<div class="video-thumbnail-container">
+							<div class="video-thumbnail-placeholder"></div>
+						</div><!-- Placeholder -->
+						<h4 class="video-title">Video Title</h4>
+						<h4>Video description...</h4>
+					</div>
+
+					<div class="col-xs-12 col-xs-4">
+						<div class="video-thumbnail-container">
+							<div class="video-thumbnail-placeholder"></div>
+						</div><!-- Placeholder -->
+						<h4 class="video-title">Video Title</h4>
+						<h4>Video description...</h4>
+					</div>
+
+					<div class="col-xs-12 col-xs-4">
+						<div class="video-thumbnail-container">
+							<div class="video-thumbnail-placeholder"></div>
+						</div><!-- Placeholder -->
+						<h4 class="video-title">Video Title</h4>
+						<h4>Video description...</h4>
+					</div>
+				</div>
+			</div>
+
+			<div class="row">
+				<div class="col-xs-12 col-sm-9">
 					<div class="col-xs-12 col-xs-4">
 						<div class="video-thumbnail-container">
 							<div class="video-thumbnail-placeholder"></div>
