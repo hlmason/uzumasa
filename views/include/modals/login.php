f0ca001abd8430e76827891f0ed7f011f143fa12
Heidi Mason <heidi@musiclaps.com>
2014-11-18 18:00:10 -0800

    Polish login modal

--- views/include/modals/login.php
+++ views/include/modals/login.php
@@ -2,25 +2,32 @@
 	<div class="modal-dialog modal-sm">
 		<div class="modal-content">
 
-			<div class="modal-header" id="login-header">
+			<div class="modal-header">
 				<button type="button" class="close" data-dismiss="modal">
 					<span aria-hidden="true">&times;</span>
 					<span class="sr-only">Close</span>
 				</button>
-				<h2 class="modal-title login-modal-heading" id="loginLabel">Log in</h2>
+				<h2 class="modal-title" id="loginLabel">Log in</h2>
 			</div>
 
-			<?php
-				include(ROOT_PATH . 'views/include/modals/social-network-login.php');
-			?>
+			<div class="modal-body">
+				<h3>Login using:</h3>
+				<ul>
+					<li><a><img class="facebook-icon" src="<?php echo BASE_URL; ?>assets/images/facebook-icon.png" alt="Facebook icon"></a></li>
+					<li><a><img class="twitter-icon" src="<?php echo BASE_URL; ?>assets/images/twitter-icon-white.png" alt="White Twitter icon"></a></li>
+					<li><a><img class="google-icon" src="<?php echo BASE_URL; ?>assets/images/google-icon.png" alt="Google icon"></a></li>
+					<li><a><img id="microsoft-icon" src="<?php echo BASE_URL; ?>assets/images/microsoft-icon.png" alt="Microsoft icon"></a></li>
+				</ul>
+				<img src="<?php echo BASE_URL; ?>assets/images/dots.png" alt="four dots">
+			</div>
 
 			<form action="#" method="post">
-				<div class="modal-body" id="login-modal-body">
-					<input type="text" name="userid" placeholder="Username">
-					<input type="password" name="password" placeholder="Password">
-					<div class="error-message">	
+				<div class="modal-body" id="username-and-password">
+					<input type="text" name="userid" placeholder=" Username">
+					<input type="password" name="password" placeholder=" Password">
+<!-- 					<div class="error-message">
 						<?= (! empty($_SESSION['login_error']) ? $_SESSION['login_error'] : '' ) ?>
-					</div>
+					</div> -->
 				</div>
 				<div class="modal-footer">
 					<a>Forgot Password</a>
