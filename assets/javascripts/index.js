179b26434f944d50faf05230b0b37611dac5812a
Heidi Mason <heidi@musiclaps.com>
2014-11-20 12:08:44 -0800

    Refactor

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -11,12 +11,12 @@ var slide_1 = $('#slide-1'); // Background image
 var nav = $('nav');
 
 $('img#slide-down-button').click(function () {
-	$('.cherry-blossoms, #youtube-player-placeholder, #slide-down-button, #youtube-player-advanced-settings').hide(); // So that YouTube player does not visibly move downward
+	$('#youtube-player-placeholder, #slide-down-button, #youtube-player-advanced-settings, .cherry-blossoms').hide(); // So that YouTube player does not visibly move downward
 	slide_1.slideUp('slow');
 	nav.removeClass('navbar-fixed-top'); // So that slide 2 does not go above navbar
 });
 $('img#slide-up-button').click(function () {
 	nav.addClass('navbar-fixed-top');
 	slide_1.slideDown('slow');
-	$('.cherry-blossoms, #youtube-player-placeholder, #slide-down-button, #youtube-player-advanced-settings').show();
+	$('#youtube-player-placeholder, #slide-down-button, #youtube-player-advanced-settings, .cherry-blossoms').show();
 });
