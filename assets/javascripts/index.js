0f936994b6e43d93d86a4a31367f9fb9152946a0
Heidi Mason <heidi@musiclaps.com>
2014-11-20 10:16:10 -0800

    Add sliding buttons

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,5 +1,17 @@
 // Initialize Skrollr
-var s = skrollr.init();
-
+var s = skrollr.init({
+    forceHeight: false // Gets rid of ending black slide
+});
 // Refresh Skrollr after resizing our sections
 s.refresh($('.homeSlide'));
+
+
+// Scrolls first slide up after 'slide down' button is clicked
+var slide_1 = $('#slide-1'); // Background image
+
+$('img#slide-down-button').click(function () {
+	slide_1.slideUp('slow');
+});
+$('img#slide-up-button').click(function () {
+	slide_1.slideDown('slow');
+});
