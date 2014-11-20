b7ef78df6236d97043b5711603796b8dbbf67470
Heidi Mason <heidi@musiclaps.com>
2014-11-20 12:01:20 -0800

    Polish sliding buttons feature further

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -11,10 +11,12 @@ var slide_1 = $('#slide-1'); // Background image
 var nav = $('nav');
 
 $('img#slide-down-button').click(function () {
+	$('.cherry-blossoms, #youtube-player-placeholder, #slide-down-button, #youtube-player-advanced-settings').hide(); // So that YouTube player does not visibly move downward
 	slide_1.slideUp('slow');
 	nav.removeClass('navbar-fixed-top'); // So that slide 2 does not go above navbar
 });
 $('img#slide-up-button').click(function () {
-	slide_1.slideDown('slow');
 	nav.addClass('navbar-fixed-top');
+	slide_1.slideDown('slow');
+	$('.cherry-blossoms, #youtube-player-placeholder, #slide-down-button, #youtube-player-advanced-settings').show();
 });
