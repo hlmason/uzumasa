dea59b9e8ed34e0bff10832febcf8e3a9bd1aabd
Heidi Mason <heidi@musiclaps.com>
2014-11-20 11:07:22 -0800

    Polish sliding buttons feature

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -8,10 +8,13 @@ s.refresh($('.homeSlide'));
 
 // Scrolls first slide up after 'slide down' button is clicked
 var slide_1 = $('#slide-1'); // Background image
+var nav = $('nav');
 
 $('img#slide-down-button').click(function () {
 	slide_1.slideUp('slow');
+	nav.removeClass('navbar-fixed-top'); // So that slide 2 does not go above navbar
 });
 $('img#slide-up-button').click(function () {
 	slide_1.slideDown('slow');
+	nav.addClass('navbar-fixed-top');
 });
