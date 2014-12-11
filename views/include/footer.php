73056e9d4e7425606143ee1af18dd2a78e7cc1cb
Heidi Mason <heidi@musiclaps.com>
2014-12-11 11:39:36 -0800

    Minify skrollr.js, revise comments

--- views/include/footer.php
+++ views/include/footer.php
@@ -11,8 +11,8 @@
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="<?php echo BASE_URL; ?>assets/javascripts/customized.bootstrap.min.js"></script>
 	<script src="<?php echo BASE_URL; ?>assets/javascripts/modal.js"></script>
-	<script src="<?php echo BASE_URL; ?>assets/javascripts/imagesloaded.js"></script><!-- Preloads background image and resizes each 'slide' to be 100% of the browser viewport (for parallax scrolling) / Needs to be above skrollr.js and _main.js to work -->
-    <script src="<?php echo BASE_URL; ?>assets/javascripts/skrollr.js"></script><!-- Stand-alone parallax scrolling (vanilla JS) library for desktop and mobile -->
+	<script src="<?php echo BASE_URL; ?>assets/javascripts/imagesloaded.js"></script><!-- Preloads background image and resizes each 'slide' to be 100% of the browser viewport (for parallax scrolling) / Needs to be above skrollr.min.js and _main.js to work -->
+    <script src="<?php echo BASE_URL; ?>assets/javascripts/skrollr.min.js"></script><!-- "Stand-alone parallax scrolling library for mobile (Android + iOS) and desktop" in vanilla JavaScript -->
     <script src="<?php echo BASE_URL; ?>assets/javascripts/_main.js"></script>
     <script src="<?php echo BASE_URL; ?>assets/javascripts/index.js"></script>
 
